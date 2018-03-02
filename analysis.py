80a638677c1c52e0b2d31f0c3f103ef0ddb9d937
Marc Greenfield <mgreenf1@stevens.edu>
2018-03-02 15:38:22 -0500

    corrected sorting error

--- analysis.py
+++ analysis.py
@@ -28,12 +28,15 @@ def check_accuracy():
 
     percent_wrong = count_of_wrong/num_matches
     accuracy = (1 - percent_wrong)*100
-    print ("These match predictions are " + str("%.2f" % accuracy) + "% accurate for " + str(num_matches) + " matches.")
+    print ("\nThese match predictions are " + str("%.2f" % accuracy) + "% accurate for " + str(num_matches) + " matches.")
 
-#Function to determine teams that upset the most
+#Function to determine teams that have the highest upset rate
 def find_underdogs():
     underdog_dict = {}
     count = 0
+    count_max = 5
+    slated_to_lose = {}
+    underdog_ratio_dict = {}
     with open('spi_matches.csv','r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         reader_list = list(reader)
@@ -48,12 +51,20 @@ def find_underdogs():
                     actual_two_win = float(match[12]) > float(match[11])
                     actual_tie = float(match[12]) == float(match[11])
                     if projected_one_win:
+                        if not match[3] in slated_to_lose:
+                            slated_to_lose[match[3]] = 1
+                        else:
+                            slated_to_lose[match[3]] += 1
                         if actual_two_win:
                             if not match[3] in underdog_dict:
                                 underdog_dict[match[3]] = 1
                             else:
                                 underdog_dict[match[3]] += 1
                     if projected_two_win:
+                        if not match[2] in slated_to_lose:
+                            slated_to_lose[match[2]] = 1
+                        else:
+                            slated_to_lose[match[2]] += 1
                         if actual_one_win:
                             if not match[2] in underdog_dict:
                                 underdog_dict[match[2]] = 1
@@ -61,12 +72,14 @@ def find_underdogs():
                                 underdog_dict[match[2]] += 1
                 except ValueError:
                     continue
-    sorted_list = sorted(underdog_dict.items(), key=lambda x: x[1], reverse = True)
-    print ("These are the top 3 teams that upset the most: ")
-    for x in sorted_list:
-            print (x[0], x[1])
+    for team in underdog_dict:
+        underdog_ratio_dict[team] = (underdog_dict[team]/slated_to_lose[team])*100
+    sorted_list = sorted(underdog_ratio_dict.items(), key=lambda x: x[1], reverse = True)
+    print ("\nThese are the " + str(count_max) + " teams that have the highest upset percentage : ")
+    for loop in sorted_list:
+            print (str(loop[0]), "with a " + str("%.2f" %loop[1]) + "% upset rate, out of " + str(slated_to_lose[loop[0]]), "matches tbey were predicted to lose.")
             count += 1
-            if count == 3:
+            if count == count_max:
                 break
 
 check_accuracy()
